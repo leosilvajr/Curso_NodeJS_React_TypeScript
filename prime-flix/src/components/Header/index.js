3afab5fce7436110f8b44e3bee7ebfb0c9057301
Leonardo Silva <leosilvatanabi@gmail.com>
2023-05-09 09:11:00 -0300

    LOGO NETFLIX

--- prime-flix/src/components/Header/index.js
+++ prime-flix/src/components/Header/index.js
@@ -4,12 +4,15 @@ import {Link} from 'react-router-dom'
 function Header(){
     return(
         <header>
-            <Link className='logo' to='/'> Prime Flix </Link>
+            <Link className='logo' to='/'>
+            <img className='logo-img' src="https://www.caviarcriativo.com/storage/2020/06/logotipo-da-netflix.jpg" alt="Chapeu" border="0"></img>
+            </Link>
             <Link className='favoritos' to='/favoritos'> Meus Filmes </Link>
         </header>
     )
 }
 
+export default Header;
 //Para renderizar o Header em todas as rotas
 /*
 Tem que importar no routes,
@@ -17,4 +20,3 @@ import Header from "./components/Header";
 Adicionar o <Header/> Acima das Rotas
  
 */
-export default Header;
