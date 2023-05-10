e8cf00bc88419445108ad06a61bf0ac1a394bb35
Leonardo <leosilvatanabi@gmail.com>
2023-05-10 08:23:42 -0300

    routes.ts / server.ts
    Criando os arquivos routes.ts / server.ts

---
+++ pizzaria/backend/src/routes.ts
@@ -0,0 +1,11 @@
+import { Router, Request, Response} from 'express';
+
+const router = Router();
+
+//Criando a rota da API         //Tipando o req e o res
+router.get('/teste', (req: Request, res: Response) => {
+    return res.json({nome: 'Site Online !'})
+});
+
+//Exportando o router para conseguir acessar o arquivo.
+export { router };
