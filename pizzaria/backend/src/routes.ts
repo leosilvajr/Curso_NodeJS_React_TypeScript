53866de66cd2c048b60ab07e3c377fbda0550763
Leonardo <leosilvatanabi@gmail.com>
2023-05-12 09:46:07 -0300

    Finalizado o Backend do projeto Pizzaria

--- pizzaria/backend/src/routes.ts
+++ pizzaria/backend/src/routes.ts
@@ -17,6 +17,7 @@ import { RemoveItemController } from './controllers/order/RemoveItemController';
 import { SendOrderController } from './controllers/order/SendOrderController';
 import { ListOrdersController } from './controllers/order/ListOrdersController';
 import { DetailOrderController } from './controllers/order/DetailOrderController';
+import { FinishOrderController } from './controllers/order/FinishOrderController';
 
 
 import { isAuthenticated } from './middlewares/isAuthenticated';
@@ -49,6 +50,7 @@ router.delete('/order/remove', isAuthenticated, new RemoveItemController().handl
 router.put('/order/send' , isAuthenticated, new SendOrderController().handle)
 router.get('/orders', isAuthenticated, new ListOrdersController().handle)
 router.get('/orders/detail', isAuthenticated, new DetailOrderController().handle)
+router.put('/orders/finish', isAuthenticated, new FinishOrderController().handle)
 
 
 //Exportando o router para conseguir acessar o arquivo.
